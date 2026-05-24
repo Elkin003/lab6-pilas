1c4151b19c765763849efdb1d99f780b9a4bb512
LMess131 <alexanderlimaabad@gmail.com>
2026-05-24 17:42:52 -0500

    Implementación de la verificacion que la tarea revertida ya no forma parte de la lista de tareas activas

--- src/Simulacion.java
+++ src/Simulacion.java
@@ -3,22 +3,43 @@ public class Simulacion {
 
         ControladorTareas ctarea = new ControladorTareas();
 
-        // 1. Asignamos 3 tareas
+        // 1. Asignamos 4 tareas
         ctarea.asignarTarea("Ejecutar Algoritmo de IA");
-        //ctarea.asignarTarea("Suspender Proceso de Backup");
+        ctarea.asignarTarea("Suspender Proceso de Backup");
         ctarea.asignarTarea("Lanzar Analisis Predictivo");
         ctarea.asignarTarea("Messi le gana a Cristiano Ronaldo");
-        System.out.println("TAREAS ASIGNDAS: (4) ");
+        System.out.println("TAREAS ASIGNADAS: (4)");
 
-        // 2. Deshacemos la ultima tarea y la pasamos a pilaRehacer
-        TareaDeServidor deshecha = ctarea.deshacer();
-        System.out.println("TAREA DESHECHA : " + deshecha.descripcion);
+        // 2. Deshacemos todas las tareas
+        TareaDeServidor deshecha1 = ctarea.deshacer();
+        System.out.println("TAREA DESHECHA: " + deshecha1.descripcion);
 
-        // 3. Verificamos por consola que ya no esta en tareas activas
-        System.out.println("VERIFICACION ");
-        System.out.println("pilaDeshacer vacia : " + ctarea.getPilaDeshacer().estaVacia());   // false = aun hay tareas activas
-        System.out.println("pilaRehacer vacia  : " + ctarea.getPilaRehacer().estaVacia());    // false = tiene la tarea revertida
-        System.out.println("\"" + deshecha.descripcion + "\" ya NO esta en tareas activas.");
-        System.out.println("\"" + deshecha.descripcion + "\" SI esta en pilaRehacer.");
+        TareaDeServidor deshecha2 = ctarea.deshacer();
+        System.out.println("TAREA DESHECHA: " + deshecha2.descripcion);
+
+        TareaDeServidor deshecha3 = ctarea.deshacer();
+        System.out.println("TAREA DESHECHA: " + deshecha3.descripcion);
+
+        TareaDeServidor deshecha4 = ctarea.deshacer();
+        System.out.println("TAREA DESHECHA: " + deshecha4.descripcion);
+
+        System.out.println("pilaDeshacer vacia : " + ctarea.getPilaDeshacer().estaVacia()); // true
+        System.out.println("pilaRehacer  vacia : " + ctarea.getPilaRehacer().estaVacia());  // false
+
+        // 3. Rehacemos todas las tareas
+        TareaDeServidor rehecha1 = ctarea.rehacer();
+        System.out.println("TAREA REHECHA: " + rehecha1.descripcion);
+
+        TareaDeServidor rehecha2 = ctarea.rehacer();
+        System.out.println("TAREA REHECHA: " + rehecha2.descripcion);
+
+        TareaDeServidor rehecha3 = ctarea.rehacer();
+        System.out.println("TAREA REHECHA: " + rehecha3.descripcion);
+
+        TareaDeServidor rehecha4 = ctarea.rehacer();
+        System.out.println("TAREA REHECHA: " + rehecha4.descripcion);
+
+        System.out.println("pilaDeshacer vacia : " + ctarea.getPilaDeshacer().estaVacia()); // false
+        System.out.println("pilaRehacer  vacia : " + ctarea.getPilaRehacer().estaVacia());  // true
     }
 }
